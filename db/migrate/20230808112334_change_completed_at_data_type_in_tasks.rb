29081806d6beca81e2f35fd8946cd16cdfeef5e1
Mignone-250 <mignone.uz@gmail.com>
2023-08-16 14:19:33 +0300

    changed completed at migration

--- db/migrate/20230808112334_change_completed_at_data_type_in_tasks.rb
+++ db/migrate/20230808112334_change_completed_at_data_type_in_tasks.rb
@@ -1,5 +1,9 @@
 class ChangeCompletedAtDataTypeInTasks < ActiveRecord::Migration[7.0]
-  def change
-    change_column :tasks, :completed_at, :date
+  def up
+    change_column :tasks, :completed_at, :date, using: 'completed_at::date'
+  end
+
+  def down
+    change_column :tasks, :completed_at, :datetime
   end
 end
